1c01a2178012301a7bed23fec33c25ea0d6bfdea
Yotoko <vhgcuong95@gmail.com>
2025-03-06 06:17:52 +0700

    Test cli

---
+++ 01_hello/hello/tests/cli.rs
@@ -0,0 +1,13 @@
+use std::process::Command;
+
+#[test]
+fn runs() {
+    let mut cmd = Command::new("ls");
+    let res = cmd.output();
+    assert!(res.is_ok());
+}
+
+#[test]
+fn works() {
+    assert!(true);
+}
