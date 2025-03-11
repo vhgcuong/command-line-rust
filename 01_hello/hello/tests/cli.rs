b397c9e22c6b90a1b1681c51c580d9a0fe8908ef
Yotoko <vhgcuong95@gmail.com>
2025-03-11 23:11:03 +0700

    #chapter1 cargo bin true false run

--- 01_hello/hello/tests/cli.rs
+++ 01_hello/hello/tests/cli.rs
@@ -1,20 +1,25 @@
-use std::process::Command;
-use assert_cmd::Command as AssertCommand;
+use assert_cmd::Command;
 
 #[test]
 fn runs() {
-    let mut cmd = Command::new("ls");
-    let res = cmd.output();
-    assert!(res.is_ok());
-}
-
-#[test]
-fn works() {
-    assert!(true);
+    let mut cmd = Command::cargo_bin("hello").unwrap();
+    cmd.assert().success().stdout("Hello, world!!!\n");
 }
 
 #[test]
 fn runs_success() {
-    let mut cmd = AssertCommand::cargo_bin("hello").unwrap();
+    let mut cmd = Command::cargo_bin("hello").unwrap();
     cmd.assert().success();
+}
+
+#[test]
+fn true_ok() {
+    let mut cmd = Command::cargo_bin("true").unwrap();
+    cmd.assert().success();
+}
+
+#[test]
+fn false_not_ok() {
+    let mut cmd = Command::cargo_bin("false").unwrap();
+    cmd.assert().failure();
 }
